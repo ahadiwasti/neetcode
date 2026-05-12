3ad5fa14cd1e220d0ae58430f8673ec32c0ab0e8
Abdul Hadi <ahadiwasti@gmail.com>
2026-05-12 15:29:16 +0400

    Add: remove-node-from-end-of-linked-list - submission-0

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    dummy := &ListNode{Next: head} 
+    slow, fast := dummy, head
+    for i := 1; i <= n ;i++{
+        fast = fast.Next
+    }
+    for fast != nil {
+        slow = slow.Next
+        fast = fast.Next
+    }
+    slow.Next = slow.Next.Next
+    return dummy.Next
+
+
+}
