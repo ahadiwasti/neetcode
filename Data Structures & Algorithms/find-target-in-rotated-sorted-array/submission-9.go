abe92b0664693d761571c9a8e2e78d1d3ebfd01f
Abdul Hadi <ahadiwasti@gmail.com>
2026-06-01 22:06:38 +0400

    Add: find-target-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-9.go
@@ -0,0 +1,23 @@
+func search(nums []int, target int) int {
+    l, r := 0, len(nums)-1
+    for l <= r {
+        mid := (r+l)/2
+        if target==nums[mid]{
+            return mid
+        }
+        if nums[l]<= nums[mid]{
+            if target < nums[l] || target > nums[mid]{
+                l = mid+1
+            }else{
+                r = mid-1
+            }
+        }else{
+            if target < nums[mid] || target > nums[r]{
+                r=mid-1
+            }else{
+                l=mid+1
+            }
+        }
+    }
+    return -1
+} 
