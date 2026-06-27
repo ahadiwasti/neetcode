b4b22f4dd6182d945c268b9f9abbdbc32706d14c
Abdul Hadi <ahadiwasti@gmail.com>
2026-06-27 22:50:47 +0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.go
@@ -0,0 +1,16 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t){
+		return false
+	}
+	counts := [26]int{}
+	for i, val := range s {
+		counts[val-'a'] +=1
+		counts[rune(t[i])-'a'] -=1
+	}
+	for _,count := range counts{
+		if count != 0{
+			return false
+		}
+	}
+	return true
+}
