c4df89718c48996356f373b13ccfa7dc0c674176
Abdul Hadi <ahadiwasti@gmail.com>
2026-06-28 22:50:42 +0400

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.go
@@ -0,0 +1,30 @@
+func threeSum(nums []int) [][]int {
+    res := [][]int{}
+    sort.Ints(nums)
+    for i:=0;i<len(nums)-2;i++{
+        if nums[i] > 0{
+            break
+        }
+        if i>0 && nums[i] == nums[i-1]{
+            continue
+        }
+        l,r:=i+1,len(nums)-1
+        for l<r{
+            threesum:= nums[i]+nums[l]+nums[r]
+            if threesum > 0{
+                r--
+            }else if threesum <0{
+                l++
+            }else{
+                res = append(res,[]int{nums[i],nums[l],nums[r]})
+                l++
+                r--
+                for l < r && nums[l] == nums[l-1]{
+                    l++
+                }
+            }
+
+        }
+    }
+    return res
+}
