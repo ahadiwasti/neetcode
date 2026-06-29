0e8ac78154a183160c2a758bbc264169f5c9ccc5
Abdul Hadi <ahadiwasti@gmail.com>
2026-06-29 18:47:16 +0400

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        freq = [0]*26
+        for idx , item in enumerate(s):
+            freq[ord(item)-ord('a')] +=1
+            freq[ord(t[idx])-ord('a')]-=1
+        for count in freq:
+            if count > 0:
+                return False
+        return True
