fad7f1144acb6001626881eaae8f67000f961598
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-05 01:23:01 +0400

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxsum,currsum = 0,0
+        l,r=0,0
+        freq = {}
+        while r < len(s):
+            char = s[r]
+            freq[char] = 1 + freq.get(char,0)
+            currsum = max(currsum,freq[char])
+
+            if (r-l+1)-currsum > k:
+                freq[s[l]] -=1
+                l+=1
+
+            maxsum = max(maxsum,r-l+1)
+            r+=1
+        return maxsum
