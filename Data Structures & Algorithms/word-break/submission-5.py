0ef8e53eb0d29713be62adf078f07c5b7076189f
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-08 01:24:39 +0400

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self,s,words):
+        seen = set(words)
+
+        n = len(s)
+        dp = [False]*(n+1)
+
+        dp[0] = True
+        for i in range(1,n+1):
+            for j in range(0,i):
+                if dp[j] and s[j:i] in seen:
+                    dp[i] = True
+                    break
+
+        return dp[n]
