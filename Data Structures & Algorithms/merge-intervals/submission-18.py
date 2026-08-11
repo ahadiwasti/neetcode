09d04265ce6617a8ec6cccab364451a37f429b75
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-11 17:39:32 +0400

    Add: merge-intervals - submission-18

---
+++ Data Structures & Algorithms/merge-intervals/submission-18.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        maxVal = max(interval[1] for interval in intervals)
+        start=end=-1
+        res = []
+        mp=[0]*(maxVal+1)
+        for mstart,mend in intervals:
+            mp[mstart] = max(mend+1,mp[mstart])
+
+        for i in range(maxVal+1):
+            if mp[i] != 0:
+                if start == -1:
+                    start = i
+                end= max(end, mp[i]-1)
+
+            if end == i:
+                res.append([start,end])
+                start=end=-1
+
+        if start != -1:
+            res.append([start,end])
+
+
+        return list(res)
