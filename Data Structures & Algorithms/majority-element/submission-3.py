5f47b686506df6da5c17fa402eb67b6312b3937f
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-19 18:55:34 +0400

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        pointer = 0
+        counter = 0
+
+        for num in nums:
+            if counter == 0:
+                pointer = num
+
+            counter += (1 if pointer == num else -1)
+
+        return pointer
