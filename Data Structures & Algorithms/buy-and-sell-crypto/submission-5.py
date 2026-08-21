5dcd6af0c072d5c7be7161e1cc4663f6d4aead8e
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-21 10:07:15 +0400

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buyat = float('inf')
+        currmax = 0
+        for p in prices:
+            if p < buyat:
+                buyat = p
+            currmax = max(currmax, p-buyat)
+
+        return currmax
+            
