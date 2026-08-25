467169d36c69212cba363411518d65639284713d
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-25 17:53:10 +0400

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        res = []
+        freq = {}
+        for num in nums:
+            freq[num] = 1+freq.get(num,0)
+
+        for key in freq:
+            if freq[key] > len(nums)//3:
+                res.append(key)
+
+        return res
