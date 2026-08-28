b7783b90e4e30e7ed90183783bf386536d0976e6
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-28 14:40:14 +0400

    Add: subarray-sum-equals-k - submission-8

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        currsum=res=0
+        seen = {0:1}
+
+        for num in nums:
+            currsum+=num
+            diff = currsum - k
+            res += seen.get(diff,0)
+            seen[currsum] = 1+seen.get(currsum,0)
+
+        return res
