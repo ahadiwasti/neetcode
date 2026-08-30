3b8c0cdd607869fc3104bb753861ba89e0ca7ad7
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-30 21:43:20 +0400

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def dfs(l,r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                else:
+                    l+=1
+                    r-=1
+            return True
+
+
+        left,right = 0,len(s)-1
+
+        while left < right:
+            if s[left] != s[right]:
+                return dfs(left+1,right) or dfs(left,right-1)
+
+            else:
+                left+=1
+                right-=1
+
+        return True
+
+        
