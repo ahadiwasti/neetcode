651cedca7c1b61d17d2cab6ab57e65aa1508ff95
Abdul Hadi <ahadiwasti@gmail.com>
2026-08-30 21:26:56 +0400

    Add: merge-strings-alternately - submission-4

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        index = 0
+        res = []
+        while index < len(word1) and index < len(word2):
+            res.append(word1[index])
+            res.append(word2[index])
+            index+=1
+        
+        res.append(word1[index:] or word2[index:])
+        return "".join(res)
