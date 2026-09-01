f2bb8b2290f42673ea5b54cd8b645ef5bb7b9b99
Abdul Hadi <ahadiwasti@gmail.com>
2026-09-01 10:55:26 +0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        def helper(char):
+            return (ord('A') <= ord(char) <= ord('Z') or ord('a') <= ord(char) <= ord('z') or ord('0') <= ord(char) <= ord('9'))
+
+        l,r = 0, len(s)-1
+        while l < r:
+            while l < r and not helper(s[l]):
+                l+=1
+                continue
+            while r > l and not helper(s[r]):
+                r-=1
+                continue
+
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l+=1
+            r-=1
+        return True
