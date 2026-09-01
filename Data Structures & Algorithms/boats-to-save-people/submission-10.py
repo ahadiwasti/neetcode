c05962cc15ead798bf81fccb0c8e2dac00ab958c
Abdul Hadi <ahadiwasti@gmail.com>
2026-09-01 09:46:54 +0400

    Add: boats-to-save-people - submission-10

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        minval,maxval = min(people),max(people)
+        freq = defaultdict(int)
+        for p in people:
+            freq[p]+=1
+
+        index = 0
+        for i in range(minval,maxval+1):
+            while freq[i]:
+                people[index] = i
+                index +=1
+                freq[i] -=1
+
+        boat = 0
+        l,r = 0 , len(people)-1
+
+        while l <= r:
+
+            diff = limit - people[r]
+            r-=1
+            boat+=1
+
+            if diff >= people[l]:
+                l+=1
+
+        return boat
+
+# T:O(n)
+# S:O(m)
+
+
