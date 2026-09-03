17a760cd23c6876fa93f422910216d7b5e5afe3f
Abdul Hadi <ahadiwasti@gmail.com>
2026-09-03 18:16:43 +0400

    Add: minimum-size-subarray-sum - submission-6

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        currsum = 0 
+        currlen = float('inf')
+        l=r=0
+        while r < len(nums):
+            currsum += nums[r]
+            while currsum >= target:
+                currsum -= nums[l]
+                currlen = min(currlen, r-l+1)
+                l+=1
+            r+=1
+        
+        return currlen if currlen != float('inf') else 0
+
+
+
