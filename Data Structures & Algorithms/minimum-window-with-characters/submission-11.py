9ed8d587931610cb580fb336febf55eb1ef1f64c
Abdul Hadi <ahadiwasti@gmail.com>
2026-09-04 09:04:05 +0400

    Add: minimum-window-with-characters - submission-11

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-11.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        window, freq = {}, {}
+        reslen = float('inf')
+        res = [-1,-1]
+
+        for c in t:
+            freq[c] = 1+freq.get(c,0)
+
+        need = len(freq)
+
+        have = l = 0
+
+        for r in range(len(s)):
+            char = s[r]
+            window[char] = 1 + window.get(char,0)
+
+            if char in freq and window[char] == freq[char]:
+                have+=1
+
+            while have == need:
+                if r-l+1 < reslen:
+                    reslen = r-l+1
+                    res = [l,r+1]
+
+                window[s[l]]-=1
+                if s[l] in freq and window[s[l]] < freq[s[l]]:
+                    have-=1
+
+                l+=1
+        i,j = res
+
+        return s[i:j] if reslen != float('inf') else ""
