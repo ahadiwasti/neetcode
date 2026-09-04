467606e1a61c38fa6dd97c864ba1b1c9760795ca
Abdul Hadi <ahadiwasti@gmail.com>
2026-09-04 09:16:25 +0400

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        window, freq = {}, {}
+
+        for c in s1:
+            freq[c] = 1+ freq.get(c,0)
+
+        for r in range(len(s2)):
+            char = s2[r]
+            window[char] = 1 + window.get(char,0)
+
+            if r >= len(s1):
+                leftchar = s2[r-len(s1)]
+                window[leftchar] -=1
+                if window[leftchar] == 0:
+                    del window[leftchar]
+
+            if r >= len(s1)-1 and freq == window:
+                return True
+
+        return False
